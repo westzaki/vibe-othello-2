32b09dbd327ee1f75e8425124fccc75e3bacd2cb
westzaki <west.zaki@gmail.com>
2026-06-13 07:44:30 +0900

    style: restore selected clang-format options

--- engine/src/version.cc
+++ engine/src/version.cc
@@ -2,6 +2,8 @@
 
 namespace vibe_othello {
 
-std::string_view engine_name() noexcept { return "vibe-othello engine"; }
+std::string_view engine_name() noexcept {
+  return "vibe-othello engine";
+}
 
 } // namespace vibe_othello
